afd3f6068a51c56dbdee40f721fe12bdebdf870d
Adriana <89477367+adriana-awa@users.noreply.github.com>
2025-01-19 12:41:17 +0100

    added format.py with function to make the string that goes in the file

---
+++ format.py
@@ -0,0 +1,38 @@
+import heapq
+
+#indexes of the priority and the rule inside the list in the questions dictionary
+P_INDEX = 0
+R_INDEX = 2
+
+#some questions do not have rules for them
+def validId(id):
+    if id not in ["experienced", "server_exp"]:
+        return True
+    return False
+
+#function to make the properly formatted string
+#for the file, with all the rules needed
+def to_string_format(questions_dict, answers_dict):
+    #1. always the same initial part
+    final_string = "#iptables firewall implementation\n*filter\n\
+:INPUT DROP [0:0]\n:FORWARD ACCEPT [0:0]\n:OUTPUT ACCEPT [0:0]"
+
+    #2. add all the rules in order
+
+    #go through the answers and SORT THEM in a priority queue
+    pq = [] #new list to heapify
+    for id in answers_dict.keys():
+        if validId(id) and answers_dict[id]: #if the answer is yes
+            #add PRIORITY and RULE to pq (as a tuple)
+            heapq.heappush(pq, (questions_dict[id][P_INDEX], questions_dict[id][R_INDEX]))
+    
+    #go through the sorted list to add the rules in the right order
+    for i in range(len(pq)):
+        final_string += heapq.heappop(pq)[R_INDEX]
+    
+
+    #3. commit
+    final_string += "COMMIT"
+
+    return final_string
+
